6313c8235376987865218072ed4e4703ab0dac0f
Radu Elisei <phoenixcreates@yahoo.com>
2023-06-19 18:04:56 +0300

    Include Product Type And Brand Entities Repo Query

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -10,9 +10,9 @@ public class StoreContext : DbContext
     {
     }
 
-    public DbSet<Product>? Products { get; set; }
-    public DbSet<ProductBrand>? ProductBrands { get; set; }
-    public DbSet<ProductType>? ProductTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
