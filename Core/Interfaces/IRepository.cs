a98f523a8062062c7b1c20735af2945690c9c9fb
Radu Elisei <phoenixcreates@yahoo.com>
2023-07-01 20:09:35 +0300

    Add SpecificationEvaluator

--- Core/Interfaces/IRepository.cs
+++ Core/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Specifications;
 
 namespace Core.Interfaces;
 
@@ -6,4 +7,6 @@ public interface IRepository<T> where T : BaseEntity
 {
     Task<T> GetByIdAsync(int id);
     Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T> GetEntityWithSpecification(ISpecification<T> specification);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification);
 }
--- Infrastructure/Data/Repository.cs
+++ Infrastructure/Data/Repository.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -20,5 +21,20 @@ public class Repository<T> : IRepository<T> where T : BaseEntity
     public async Task<IReadOnlyList<T>> ListAllAsync()
     {
         return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetEntityWithSpecification(ISpecification<T> specification)
+    {
+        return await ApplySpecification(specification).FirstOrDefaultAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+    {
+        return await ApplySpecification(specification).ToListAsync();
+    }
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+    {
+        return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable() , specification);
     }
 }
