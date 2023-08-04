79852649a4b08e255daa35d1c1d01529726cac06
Radu Elisei <radu.elisei@yahoo.com>
2023-08-04 16:43:52 +0300

    Add Search

--- Core/Specifications/ProductSpecificationParams.cs
+++ Core/Specifications/ProductSpecificationParams.cs
@@ -14,4 +14,10 @@ public class ProductSpecificationParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string Sort { get; set; }
+    private string _search;
+    public string Search
+    {
+        get => _search;
+        set => _search = value.ToLower();
+    }
 }
